7baefb09a31856d92926e28b078df1ba843533e3
Edward Bridges <github@eqbridges.com>
2017-11-15 06:58:01 -0500

    add chart of accounts report

--- accounting_reports/accounting_reports.py
+++ accounting_reports/accounting_reports.py
@@ -2,10 +2,12 @@
 Accounting Reports
 
 Usage:
+  accounting-reports chart-of-accounts [--db=<PATH>]
   accounting-reports -h | --help
   accounting-reports --version
   accounting-reports --verbose
 Options:
+  --db=<PATH>   Path to SQLite file [default: ~/Transactions/accounting-books.db.gnucash].
   --verbose     Verbose logging.
   -h --help     Show this screen.
   --version     Show version.
@@ -15,6 +17,14 @@ from .version import __version__
 
 from logging import basicConfig, INFO, DEBUG
 from docopt import docopt
+from piecash import open_book
+
+
+def chart_of_accounts(db):
+  with open_book('../accounting-books.db.gnucash') as book:
+      for acc in book.accounts:
+          if(acc.code):
+            print('%d\t%s\t%s' % (int(acc.code), acc.type, acc.fullname))
 
 
 def main():
@@ -23,3 +33,8 @@ def main():
     basicConfig(level=DEBUG)
   else:
     basicConfig(level=INFO)
+
+  db_file = args['--db']
+
+  if(args['chart-of-accounts']):
+    chart_of_accounts(db_file)
