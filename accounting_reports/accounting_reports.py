6445ffc93625a66a0c4bf8e6e8238ab87e5c961a
Edward Bridges <github@eqbridges.com>
2018-01-31 15:47:38 -0500

    pylint fixes

--- accounting_reports/accounting_reports.py
+++ accounting_reports/accounting_reports.py
@@ -4,6 +4,7 @@ Accounting Reports
 Usage:
   accounting-reports chart-of-accounts --db=<PATH> [--output=<FORMAT>] [--verbose]
   accounting-reports balances --db=<PATH> [--accounts=<ACCOUNTS>] [--begin=<BEGIN_DATE>] [--end=<END_DATE>] [--output=<FORMAT>] [--verbose]
+  accounting-reports budget --db=<PATH> [--accounts=<ACCOUNTS>] [--begin=<BEGIN_DATE>] [--end=<END_DATE>] [--output=<FORMAT>] [--verbose]
   accounting-reports -h | --help
   accounting-reports --version
   accounting-reports --verbose
@@ -18,29 +19,44 @@ Options:
   --version             Show version.
 '''
 
-from .version import __version__
-from .util import configure_logging, csv_to_list, filter_list, begin_or_default, end_or_default, output_arg
+from decimal import Decimal
 from logging import info, debug
 from docopt import docopt
 from piecash import open_book
-from decimal import Decimal
+
+from .version import __version__
+from .util import (configure_logging, csv_to_list, filter_list, begin_or_default,
+                   end_or_default, output_arg)
 
 
-def account_balances(db, accounts, begin, end, output_func):
-  debug('account_balance called with [%s] [%s] [%s--%s]' % (db, accounts, begin, end))
-  balances = []
-  with open_book(db) as book:
+def budget_report(database, accounts, begin, end, output_func):
+  """
+  Prints a report for the given accounts and whether they're over or under budget.
+  """
+  debug('budget_report called with [%s] [%s] [%s--%s]' % (database, accounts, begin, end))
+  with open_book(database) as book:
+    acctlist = filter_list(book.accounts, accounts)
+
+def account_balances(database, accounts, begin, end, output_func):
+  """
+  Prints the balances for the given accounts in the specified format.
+  """
+  debug('account_balance called with [%s] [%s] [%s--%s]' % (database, accounts, begin, end))
+  with open_book(database) as book:
     acctlist = filter_list(book.accounts, accounts)
     for account in acctlist:
       result = {
-        'account_code' : account.code if account.code else None,
-        'account_name' : account.fullname,
-        'balance' : balance_of(account, begin, end)
+          'account_code' : account.code if account.code else None,
+          'account_name' : account.fullname,
+          'balance' : balance_of(account, begin, end)
       }
       output_func(result)
 
 
 def balance_of(account, begin, end):
+  """
+  Returns the balance of the given account over the given date range.
+  """
   debug('balance_of account:[%s] over[%s--%s]' % (account, begin, end))
   balance = 0
   if end:
@@ -57,17 +73,23 @@ def balance_of(account, begin, end):
 
 
 def chart_of_accounts(db, output_func):
+  """
+  Outputs the chart of accounts for the given book of accounts.
+  """
   with open_book(db) as book:
     for account in book.accounts:
       result = {
-        'account_code' : int(account.code) if account.code else None,
-        'account_type' : account.type,
-        'account_name' : account.fullname,
+          'account_code' : int(account.code) if account.code else None,
+          'account_type' : account.type,
+          'account_name' : account.fullname,
       }
       output_func(result)
 
 
 def main():
+  '''
+  Application entry point.
+  '''
   args = docopt(__doc__, version=__version__)
   configure_logging(args['--verbose'])
 
@@ -76,10 +98,13 @@ def main():
   begin = begin_or_default(args['--begin'])
   end = end_or_default(args['--end'])
 
-  output_func=output_arg(args['--output'])
+  output_func = output_arg(args['--output'])
 
-  if(args['chart-of-accounts']):
+  if args['chart-of-accounts']:
     chart_of_accounts(db_file, output_func)
 
-  if(args['balances']):
+  if args['balances']:
     account_balances(db_file, accounts, begin, end, output_func)
+
+  if args['budget']:
+    budget_report(db_file, accounts, begin, end, output_func)
--- accounting_reports/util.py
+++ accounting_reports/util.py
@@ -8,40 +8,61 @@ from decimal import Decimal
 
 
 def output_json(values):
+  """
+  Formats the given data structure as JSON.
+  """
   print(dumps(values, cls=DecimalEncoder))
 
 
 def output_csv(values):
+  """
+  Formats the given data structure as CSV.
+  """
   w = DictWriter(stdout, values.keys())
   w.writerow(values)
 
 
 def output_arg(val):
+  """
+  Returns the proper output function given the input.
+  """
   return {
-    'csv' : output_csv,
-    'json': output_json,
+      'csv' : output_csv,
+      'json': output_json,
   }[val]
 
 
 class DecimalEncoder(JSONEncoder):
+  """
+  Ensures floats are properly encoded.
+  """
   def default(self, o):
+    """
+    Returns an instance of the proper encoder.
+    """
     if isinstance(o, Decimal):
       return float(o)
     return super(DecimalEncoder, self).default(o)
 
 
 def configure_logging(level):
+  """
+  Configures logging to INFO if the level is not present, else to DEBUG.
+  """
   if not level:
     level = INFO
   else:
     level = DEBUG
   basicConfig(
-    format='[%(asctime)s][%(levelname)s] %(message)s',
-    datefmt='%Y/%m/%d %H:%M:%S',
-    level=level)
+      format='[%(asctime)s][%(levelname)s] %(message)s',
+      datefmt='%Y/%m/%d %H:%M:%S',
+      level=level)
 
 
 def csv_to_list(val):
+  """
+  Converts the given comma separated list to an array; if `val` is None, returns an empty array.
+  """
   if val:
     return val.split(',')
   else:
@@ -49,6 +70,10 @@ def csv_to_list(val):
 
 
 def begin_or_default(val):
+  """
+  Returns the first day of this year if `val` is None, else returns the given string formatted 
+  as a `date` instance.
+  """
   if val:
     d = strptime(val, '%Y-%m-%d')
     debug(d)
@@ -59,10 +84,19 @@ def begin_or_default(val):
 
 
 def end_or_default(val):
+  """
+  If `val` is empty returns the first day of this month, else returns `val` formatted as a date.
+
+  Args:
+      val: A date formatted as `%Y-%m-%d`. May be None.
+
+  Returns:
+      A `date` instance.
+  """
   if val:
-    d = strptime(val, '%Y-%m-%d')
-    debug(d)
-    return date(d[0], d[1], d[2])
+    parsed_date = strptime(val, '%Y-%m-%d')
+    debug(parsed_date)
+    return date(parsed_date[0], parsed_date[1], parsed_date[2])
   else:
     today = date.today()
     # return first day of this month, because comparison is
@@ -70,9 +104,19 @@ def end_or_default(val):
     return date(today.year, today.month, 1)
 
 
-# return all accounts if filtered is empty, else return acounts named in filtered_accounts
 def filter_list(all_accounts, filtered_accounts):
-  if not filtered_accounts or len(filtered_accounts) == 0:
+  """
+  Returns all accounts if filtered is empty, else return acounts named in filtered_accounts
+
+  Args:
+      all_accounts: The list of Account objects to scan.
+      filtered_accounts: Account names to filter `all_accounts` with.
+
+  Returns:
+      `all_accounts` if `filtered_accounts` is empty. Else a new list of Account
+      objects whose `fullname` matches the list given in `filtered_accounts`.
+  """
+  if not filtered_accounts:
     return all_accounts
   else:
     accounts = []
