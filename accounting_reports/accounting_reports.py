19da9941968bfcbdf1996e1699842088cb8c49ba
Edward Bridges <github@eqbridges.com>
2018-02-10 16:03:52 -0500

    whitespace

--- accounting_reports/accounting_reports.py
+++ accounting_reports/accounting_reports.py
@@ -5,21 +5,25 @@ Usage:
   accounting-reports chart-of-accounts --db=<PATH> [--output=<FORMAT>] [--verbose]
   accounting-reports balances --db=<PATH> [--accounts=<ACCOUNTS>] [--begin=<BEGIN_DATE>]
                      [--end=<END_DATE>] [--output=<FORMAT>] [--verbose]
-  accounting-reports budget --db=<PATH> [--budget-account=<ACCOUNTS> | --actual-account=<ACCOUNTS>]
-                     [--begin=<BEGIN_DATE>] [--output=<FORMAT>] [--verbose]
+  accounting-reports budget --db=<PATH> [--begin=<BEGIN_DATE>]
+                     [--budget-accounts=<ACCOUNTS>] [--actual-accounts=<ACCOUNTS>]
+                     [--output=<FORMAT>] [--verbose]
   accounting-reports -h | --help
   accounting-reports --version
   accounting-reports --verbose
 
 Options:
-  --db=<PATH>           Path to SQLite file.
-  --accounts=<ACCOUNTS> Comma separated list of accounts to get balances for. Default: all.
-  --begin=<BEGIN_DATE>  Begin date of balances (yyyy-mm-dd).  Default: first day of the year.
-  --end=<END_DATE>      Date to get balances as-of (yyyy-mm-dd).  Default: last day of previous month.
-  --output=<FORMAT>     Format to output results in (csv, json). [Default: csv]
-  --verbose             Verbose logging.
-  -h --help             Show this screen.
-  --version             Show version.
+  --db=<PATH>                  Path to SQLite file.
+  --accounts=<ACCOUNTS>        Comma separated list of accounts. Default: all.
+  --actual-accounts=<ACCOUNTS> Comma separated list of actual accounts. Required.
+  --budget-accounts=<ACCOUNTS> Comma separated list of budget accounts. Required.
+  --begin=<BEGIN_DATE>         Begin date of balances (yyyy-mm-dd).  Default: first day of the year.
+  --end=<END_DATE>             Date to get balances as-of (yyyy-mm-dd).  Default: last day of
+                               previous month.
+  --output=<FORMAT>            Format to output results in (csv, json). [Default: csv]
+  --verbose                    Verbose logging.
+  -h --help                    Show this screen.
+  --version                    Show version.
 '''
 
 from decimal import Decimal
