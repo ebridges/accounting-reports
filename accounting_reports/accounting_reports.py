9ed41a5347dc434219a5f327a08c93806b1908a2
Edward Bridges <github@eqbridges.com>
2019-04-01 21:23:54 -0400

    conform to pep8 4 space indent

--- accounting_reports/accounting_reports.py
+++ accounting_reports/accounting_reports.py
@@ -38,155 +38,155 @@ from util import (configure_logging, csv_to_list, filter_list, begin_or_default,
 
 
 def display_accounts(database, accounts, open_if_lock=False):
-  """
-  Prints out detailed information about the given accounts for debugging purposes.
-  """
-  debug('displaying accounts [%s]' % (accounts))
-  with open_book(database, open_if_lock=open_if_lock) as book:
-    account_list = filter_list(book.accounts, accounts)
-    for account in account_list:
-      print('account name: %s' % account.name)
-      print('account type: %s' % account.type)
-      print('account sign: %s' % account.sign)
-      pprint(vars(account))
-      print('='*50)
-      # display the first 10 splits
-      for split in account.splits[:10]:
-        print('split transaction: %s' % split.transaction.description)
-        print('split post date: %s' % split.transaction.post_date)
-        print('split amount: %s' % split_value(split))
-        pprint(vars(split))
-        print('-'*50)
+    """
+    Prints out detailed information about the given accounts for debugging purposes.
+    """
+    debug('displaying accounts [%s]' % (accounts))
+    with open_book(database, open_if_lock=open_if_lock) as book:
+        account_list = filter_list(book.accounts, accounts)
+        for account in account_list:
+            print('account name: %s' % account.name)
+            print('account type: %s' % account.type)
+            print('account sign: %s' % account.sign)
+            pprint(vars(account))
+            print('='*50)
+            # display the first 10 splits
+            for split in account.splits[:10]:
+                print('split transaction: %s' % split.transaction.description)
+                print('split post date: %s' % split.transaction.post_date)
+                print('split amount: %s' % split_value(split))
+                pprint(vars(split))
+                print('-'*50)
 
 
 def budget_report(database, accounts, begin, end, output_func):
-  """
-  Prints a report for the given accounts with the budgeted amount and the actual balance.
-  """
-  debug('budget_report called with [%s] [%s]' % (database, begin))
-  with open_book(database, open_if_lock=True) as book:
-    all_accounts = book.accounts
-    acctlist = filter_list(all_accounts, accounts)
-    datelist = list_of_months_from(begin, end)
+    """
+    Prints a report for the given accounts with the budgeted amount and the actual balance.
+    """
+    debug('budget_report called with [%s] [%s]' % (database, begin))
+    with open_book(database, open_if_lock=True) as book:
+        all_accounts = book.accounts
+        acctlist = filter_list(all_accounts, accounts)
+        datelist = list_of_months_from(begin, end)
 
-    for end in datelist:
-      for account in acctlist:
-        (budget_balance, actual_balance) = budget_balance_of(account, begin, end.date())
+        for end in datelist:
+            for account in acctlist:
+                (budget_balance, actual_balance) = budget_balance_of(account, begin, end.date())
 
-        result = {
-            'date' : end.date().strftime('%Y-%m'),
-            'account_code' : account.code if account.code else None,
-            'account' : account.fullname,
-            'budget_balance': budget_balance,
-            'actual_balance': actual_balance
-        }
-        output_func(result)
+                result = {
+                    'date' : end.date().strftime('%Y-%m'),
+                    'account_code' : account.code if account.code else None,
+                    'account' : account.fullname,
+                    'budget_balance': budget_balance,
+                    'actual_balance': actual_balance
+                }
+                output_func(result)
 
 
 def account_balances(database, accounts, begin, end, output_func):
-  """
-  Prints the balances for the given accounts in the specified format.
-  """
-  debug('account_balance called with [%s] [%s] [%s--%s]' % (database, accounts, begin, end))
-  with open_book(database) as book:
-    acctlist = filter_list(book.accounts, accounts)
-    for account in acctlist:
-      result = {
-          'account_code' : account.code if account.code else None,
-          'account_name' : account.fullname,
-          'balance' : balance_of(account, begin, end)
-      }
-      output_func(result)
+    """
+    Prints the balances for the given accounts in the specified format.
+    """
+    debug('account_balance called with [%s] [%s] [%s--%s]' % (database, accounts, begin, end))
+    with open_book(database) as book:
+        acctlist = filter_list(book.accounts, accounts)
+        for account in acctlist:
+            result = {
+                'account_code' : account.code if account.code else None,
+                'account_name' : account.fullname,
+                'balance' : balance_of(account, begin, end)
+            }
+            output_func(result)
 
 
 def budget_balance_of(account, begin, end):
-  """
-  Returns a tuple of (budgeted,actual) amounts for the given budget account.
-  """
-  budget_balance = 0
-  actual_balance = 0
-  for split in account.splits:
-    transaction = split.transaction
-    post_date = transaction.post_date.date()
-    if post_date >= begin and post_date <= end:
-      debug('post_date (%s) is between (%s--%s)' % (post_date, begin, end))
-      if split.value >= 0:
-        budget_balance += split.value
-      if split.value < 0:
-        actual_balance += split.value
+    """
+    Returns a tuple of (budgeted,actual) amounts for the given budget account.
+    """
+    budget_balance = 0
+    actual_balance = 0
+    for split in account.splits:
+        transaction = split.transaction
+        post_date = transaction.post_date.date()
+        if post_date >= begin and post_date <= end:
+            debug('post_date (%s) is between (%s--%s)' % (post_date, begin, end))
+            if split.value >= 0:
+                budget_balance += split.value
+            if split.value < 0:
+                actual_balance += split.value
 
-  budget_balance = Decimal(budget_balance)
-  actual_balance = Decimal(actual_balance)
-  debug('account:[%s] over [%s--%s]: (%d/%d)' %
-        (account.fullname, begin, end, budget_balance, actual_balance))
-  return (budget_balance.quantize(Decimal('0.01')), actual_balance.quantize(Decimal('0.01')))
+    budget_balance = Decimal(budget_balance)
+    actual_balance = Decimal(actual_balance)
+    debug('account:[%s] over [%s--%s]: (%d/%d)' %
+          (account.fullname, begin, end, budget_balance, actual_balance))
+    return (budget_balance.quantize(Decimal('0.01')), actual_balance.quantize(Decimal('0.01')))
 
 
 def balance_of(account, begin, end):
-  """
-  Returns the balance of the given account over the given date range.
-  """
-  balance = 0
-  if end:
-    for split in account.splits:
-      transaction = split.transaction
-      post_date = transaction.post_date.date()
-      if post_date >= begin and post_date <= end:
-        debug('post_date (%s) is between (%s--%s)' % (post_date, begin, end))
-        balance += split.value * account.sign
-  else:
-      balance = account.get_balance()
-  balance = Decimal(balance)
-  debug('balance_of account:[%s] over [%s--%s]: [%d]' % (account.fullname, begin, end, balance))
-  return balance.quantize(Decimal('0.01'))
+    """
+    Returns the balance of the given account over the given date range.
+    """
+    balance = 0
+    if end:
+        for split in account.splits:
+            transaction = split.transaction
+            post_date = transaction.post_date.date()
+            if post_date >= begin and post_date <= end:
+                debug('post_date (%s) is between (%s--%s)' % (post_date, begin, end))
+                balance += split.value * account.sign
+    else:
+        balance = account.get_balance()
+    balance = Decimal(balance)
+    debug('balance_of account:[%s] over [%s--%s]: [%d]' % (account.fullname, begin, end, balance))
+    return balance.quantize(Decimal('0.01'))
 
 
 def chart_of_accounts(database, output_func):
-  """
-  Outputs the chart of accounts for the given book of accounts.
-  """
-  with open_book(database) as book:
-    for account in sorted(book.accounts,
-                          key=lambda account: int(account.code) if account.code else 0):
-      result = {
-          'account_code' : int(account.code) if account.code else None,
-          'account_type' : account.type,
-          'account_name' : account.fullname,
-      }
-      output_func(result)
+    """
+    Outputs the chart of accounts for the given book of accounts.
+    """
+    with open_book(database) as book:
+        for account in sorted(book.accounts,
+                              key=lambda account: int(account.code) if account.code else 0):
+            result = {
+                'account_code' : int(account.code) if account.code else None,
+                'account_type' : account.type,
+                'account_name' : account.fullname,
+            }
+            output_func(result)
 
 
 def main():
-  """
-  Application entry point.
-  """
-  args = docopt(__doc__, version=__version__)
-  configure_logging(args['--verbose'])
+    """
+    Application entry point.
+    """
+    args = docopt(__doc__, version=__version__)
+    configure_logging(args['--verbose'])
 
-  db_file = args['--db']
-  begin = begin_or_default(args['--begin'])
-  end = end_or_default(args['--end'])
+    db_file = args['--db']
+    begin = begin_or_default(args['--begin'])
+    end = end_or_default(args['--end'])
 
-  if os.path.isfile(args['--accounts']):
-    accounts = read_list_from_file(args['--accounts'])
-  else:
-    accounts = csv_to_list(args['--accounts'])
+    if os.path.isfile(args['--accounts']):
+        accounts = read_list_from_file(args['--accounts'])
+    else:
+        accounts = csv_to_list(args['--accounts'])
 
-  output_func = output_arg(args['--output'])
+    output_func = output_arg(args['--output'])
 
-  info('accounting-reports called with args: [%s]' % args)
-  if args['chart-of-accounts']:
-    chart_of_accounts(db_file, output_func)
+    info('accounting-reports called with args: [%s]' % args)
+    if args['chart-of-accounts']:
+        chart_of_accounts(db_file, output_func)
 
-  if args['balances']:
-    account_balances(db_file, accounts, begin, end, output_func)
+    if args['balances']:
+        account_balances(db_file, accounts, begin, end, output_func)
 
-  if args['budget']:
-    budget_report(db_file, accounts, begin, end, output_func)
+    if args['budget']:
+        budget_report(db_file, accounts, begin, end, output_func)
 
-  if args['display-accounts']:
-    open_if_locked = args['--open-if-locked']
-    display_accounts(db_file, accounts, open_if_locked)
+    if args['display-accounts']:
+        open_if_locked = args['--open-if-locked']
+        display_accounts(db_file, accounts, open_if_locked)
 
 
 if __name__ == '__main__':
--- accounting_reports/util.py
+++ accounting_reports/util.py
@@ -13,154 +13,155 @@ from pathlib import Path
 
 
 def output_json(values):
-  """
-  Formats the given data structure as JSON.
-  """
-  print(dumps(values, cls=DecimalEncoder))
+    """
+    Formats the given data structure as JSON.
+    """
+    print(dumps(values, cls=DecimalEncoder))
 
 
 def output_csv(values):
-  """
-  Formats the given data structure as CSV.
-  """
-  writer = DictWriter(stdout, values.keys())
-  writer.writerow(values)
+    """
+    Formats the given data structure as CSV.
+    """
+    writer = DictWriter(stdout, values.keys())
+    writer.writerow(values)
 
 
 def output_arg(val):
-  """
-  Returns the proper output function given the input.
-  """
-  return {
-      'csv' : output_csv,
-      'json': output_json,
-  }[val]
+    """
+    Returns the proper output function given the input.
+    """
+    return {
+        'csv' : output_csv,
+        'json': output_json,
+    }[val]
 
 
 class DecimalEncoder(JSONEncoder):
-  """
-  Ensures floats are properly encoded.
-  """
-  def default(self, o):
     """
-    Returns an instance of the proper encoder.
+    Ensures floats are properly encoded.
     """
-    if isinstance(o, Decimal):
-      return float(o)
-    return super(DecimalEncoder, self).default(o)
+    def default(self, o):
+        """
+        Returns an instance of the proper encoder.
+        """
+        if isinstance(o, Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
 
 
 def configure_logging(level):
-  """
-  Configures logging to INFO if the level is not present, else to DEBUG.
-  """
-  if not level:
-    level = INFO
-  else:
-    level = DEBUG
-  basicConfig(
-      format='[%(asctime)s][%(levelname)s] %(message)s',
-      datefmt='%Y/%m/%d %H:%M:%S',
-      level=level)
+    """
+    Configures logging to INFO if the level is not present, else to DEBUG.
+    """
+    if not level:
+        level = INFO
+    else:
+        level = DEBUG
+    basicConfig(
+        format='[%(asctime)s][%(levelname)s] %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S',
+        level=level)
 
 
 def csv_to_list(val):
-  """
-  Converts the given comma separated list to an array; if `val` is None, returns an empty array.
-  """
-  if val:
-    return val.split(',')
-  else:
-    return []
+    """
+    Converts the given comma separated list to an array; if `val` is None, returns an empty array.
+    """
+    if val:
+        return val.split(',')
+    else:
+        return []
 
 
 def list_of_months_from(begin, end=datetime.now()):
-  """
-  Returns a list of date objects beginning at the given `begin` up to the current month end.
-  """
-  from_date = first_day_of_month(begin)
-  until = datetime(end.year, end.month, end.day)
-  dates = map(last_day_of_month,
-              [dt for dt in rrule(MONTHLY, dtstart=from_date, bymonthday=1, until=until)])
-  return list(dates)
+    """
+    Returns a list of date objects beginning at the given `begin` up to the current month end.
+    """
+    from_date = first_day_of_month(begin)
+    until = datetime(end.year, end.month, end.day)
+    dates = map(last_day_of_month,
+                [dt for dt in rrule(MONTHLY, dtstart=from_date, bymonthday=1, until=until)])
+    return list(dates)
 
 
 def first_day_of_month(val):
-  """
-  Converts the given val to the first day of the given month.
-  """
-  return datetime(val.year, val.month, 1)
+    """
+    Converts the given val to the first day of the given month.
+    """
+    return datetime(val.year, val.month, 1)
 
 
 def last_day_of_month(begin_date):
-  """
-  Converts the given begin_date to the last day of the given month.
-  """
-  if begin_date.month == 12:
-      return begin_date.replace(day=31)
-  return begin_date.replace(month=begin_date.month+1, day=1) - timedelta(days=1)
+    """
+    Converts the given begin_date to the last day of the given month.
+    """
+    if begin_date.month == 12:
+        return begin_date.replace(day=31)
+    return begin_date.replace(month=begin_date.month+1, day=1) - timedelta(days=1)
 
 
 def begin_or_default(val):
-  """
-  Returns the first day of this year if `val` is None, else returns the given string formatted
-  as a `date` instance.
-  """
-  if val:
-    parsed_date = strptime(val, '%Y-%m-%d')
-    return date(parsed_date[0], parsed_date[1], parsed_date[2])
-  else:
-    today = date.today()
-    return date(today.year, 1, 1)
+    """
+    Returns the first day of this year if `val` is None, else returns the given string formatted
+    as a `date` instance.
+    """
+    if val:
+        parsed_date = strptime(val, '%Y-%m-%d')
+        return date(parsed_date[0], parsed_date[1], parsed_date[2])
+    else:
+        today = date.today()
+        return date(today.year, 1, 1)
 
 
 def end_or_default(val):
-  """
-  If `val` is empty returns the last day of this month, else returns `val` formatted as a date.
+    """
+    If `val` is empty returns the last day of this month, else returns `val` formatted as a date.
 
-  Args:
-      val: A date formatted as `%Y-%m-%d`. May be None.
+    Args:
+        val: A date formatted as `%Y-%m-%d`. May be None.
 
-  Returns:
-      A `date` instance.
-  """
-  if val:
-    parsed_date = strptime(val, '%Y-%m-%d')
-    debug(parsed_date)
-    return date(parsed_date[0], parsed_date[1], parsed_date[2])
-  else:
-    today = date.today()
-    return last_day_of_month(today)
+    Returns:
+        A `date` instance.
+    """
+    if val:
+        parsed_date = strptime(val, '%Y-%m-%d')
+        debug(parsed_date)
+        return date(parsed_date[0], parsed_date[1], parsed_date[2])
+    else:
+        today = date.today()
+        return last_day_of_month(today)
 
 
 def filter_list(all_accounts, filtered_accounts):
-  """
-  Returns all accounts if filtered is empty, else return accounts named in filtered_accounts
+    """
+    Returns all accounts if filtered is empty, else return accounts named in filtered_accounts
 
-  Args:
-      all_accounts: The list of Account objects to scan.
-      filtered_accounts: Account names to filter `all_accounts` with.
+    Args:
+        all_accounts: The list of Account objects to scan.
+        filtered_accounts: Account names to filter `all_accounts` with.
 
-  Returns:
-      `all_accounts` if `filtered_accounts` is empty. Else a new list of Account
-      objects whose `fullname` matches the list given in `filtered_accounts`.
-  """
-  if not filtered_accounts:
-    return all_accounts
-  else:
-    accounts = []
-    for account in all_accounts:
-      if account.fullname in filtered_accounts:
-        accounts.append(account)
-    return accounts
+    Returns:
+        `all_accounts` if `filtered_accounts` is empty. Else a new list of Account
+        objects whose `fullname` matches the list given in `filtered_accounts`.
+    """
+    if not filtered_accounts:
+        return all_accounts
+    else:
+        accounts = []
+        for account in all_accounts:
+            if account.fullname in filtered_accounts:
+                accounts.append(account)
+        return accounts
+
 
 def split_value(split):
-  """
-  Formats amount of the split.
-  """
-  return Decimal(split.value.quantize(Decimal('.01'), rounding=ROUND_HALF_UP))
+    """
+    Formats amount of the split.
+    """
+    return Decimal(split.value.quantize(Decimal('.01'), rounding=ROUND_HALF_UP))
 
 
 def read_list_from_file(filename):
-  p = Path(filename)
-  return p.read_text().splitlines()
+    p = Path(filename)
+    return p.read_text().splitlines()
